07e9ad698c70aafb99c64367b567c70b4d500731
Philip Davis <philipcdavis11@gmail.com>
2021-01-11 17:30:40 -0500

    Linear gradient on text

--- GradientText/ContentView.swift
+++ GradientText/ContentView.swift
@@ -3,14 +3,24 @@
 //  GradientText
 //
 //  Created by Philip Davis on 1/11/21.
-//
+//  https://swiftui.design/guide
 
 import SwiftUI
 
 struct ContentView: View {
+    let text = Text("SwiftUI is a good tool for Designers").font(Font.largeTitle.bold())
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        text
+            .foregroundColor(.clear)
+            .overlay(
+                LinearGradient(
+                    gradient: Gradient(colors: [.orange, .red, .purple, .blue]),
+                    startPoint: .trailing,
+                    endPoint: .leading
+                )
+                .mask(text))
+            .padding(16)
+        
     }
 }
 
