feae50e9a8005b542b44a0dd23c9505236e3a451
Philip Davis <philipcdavis11@gmail.com>
2021-01-11 15:30:23 -0500

    Initial Commit

---
+++ GradientText/GradientTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientTextApp.swift
+//  GradientText
+//
+//  Created by Philip Davis on 1/11/21.
+//
+
+import SwiftUI
+
+@main
+struct GradientTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
